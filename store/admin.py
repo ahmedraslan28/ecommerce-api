01039710f27b9b49a43a349e688389cbad9d980f
ahmed <ahmedraslan28@gmail.com>
2023-02-26 12:32:10 +0200

    building the data model

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from . import models
 # Register your models here.
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Product)
+admin.site.register(models.Order)
+admin.site.register(models.Customer)
+admin.site.register(models.Collection)
