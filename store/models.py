ec1629145a1b01044b7c175347ea7472655b5c8a
ahmed <ahmedraslan28@gmail.com>
2023-03-14 14:45:30 +0200

    modify image field in productimage table set it to null and blank true

--- store/models.py
+++ store/models.py
@@ -53,7 +53,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='store/images')
+    image = models.ImageField(upload_to='store/images', null=True, blank=True)
 
 
 class Customer(models.Model):
